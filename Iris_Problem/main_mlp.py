ca49ac2ae78234c61955bf945210ff806afa811a
Ulises PH <pedro.ul@hotmail.com>
2016-10-18 13:36:03 -0200

    Correção de um bug

--- Iris_Problem/main_mlp.py
+++ Iris_Problem/main_mlp.py
@@ -21,6 +21,9 @@ class Neuron:
         return [w.value for w in self.weights]
 
     def linear_combination(self,entries):
+        #print("<<")
+        #[print("LC",entry,weight.value) for entry,weight in zip(entries,self.weights)]
+        #print(">>")        
         return sum([(entry*weight.value) for entry,weight in zip(entries,self.weights)])
     
     def activation(self,value):
@@ -81,6 +84,7 @@ class Layer:
         return outputs 
  
     def calculate_error(self,targets,entries):
+        #print("ENTRIES",entries,self.name)
         self.entries = entries
         outputs = [neuron.calculate_error(self.entries,target) for neuron,target in zip(self.neurons,targets)]
         self.error_total = sum(outputs)        
@@ -95,22 +99,23 @@ class Layer:
         return weights
         
     def update_output_weights(self,targets):
+        #print(self.name)
         [neuron.update_output_weights(self.entries,target) for neuron,target in zip(self.neurons,targets)]
     
     def update_hidden_weights(self,front_layer):
         front_deltas = front_layer.get_deltas()
         front_weights = np.array(front_layer.get_weights())
         
-        print(front_deltas,front_weights)        
+        #print(front_deltas,front_weights,len(self.neurons),self.entries)        
         
         cont = 1
+        
         for neuron in self.neurons:
-            front_weights[:,cont]
-            neuron.update_hidden_weights(front_deltas,front_weights[:,cont],self.entries[cont])
+            for entry in self.entries:
+                neuron.update_hidden_weights(front_deltas,front_weights[:,cont],entry)
             cont = cont + 1
-class OutputLayer(Layer):
-    def __init__(self,neurons):
-        Layer.__init__(self,neurons)
+    def update_weights(self):
+        [neuron.update_weights() for neuron in self.neurons]
     
 class Network:
     
@@ -139,7 +144,7 @@ class Network:
         neurons = []
         for x in range(n_neurons):
             weights = []
-            for y in range(n_entries):
+            for y in range(n_entries+1):#MAIS UM POR CAUSA DO PESO DO BIAS
                 weights.append(Weight(0.5))
                 
             neuton = Neuron(weights)
@@ -153,18 +158,45 @@ class Network:
             
         return neurons  
     
-    def execute(self):
-        [print(layer.name) for layer in self.layers]
+    def execute(self,inputs,targets):
+        #[print(layer.name) for layer in self.layers]
+                     
+        entries = [1]+self.layers[0].calculate_output(inputs)#CAMADA DE ENTRADA RECEBE COMO ENTRADA OS EXEMPLOS
+        for x in range(1,len(self.layers)-1):
+            entries = [1]+self.layers[x].calculate_output(entries)  
         
-    def bakcpropagation(self):
-        pass
-    
+        final_error = self.layers[-1].calculate_error(targets,entries)#RECEBE OS TARGETS PARA CALCULAR O ERRO
+        
+        print("ERRO FINAL",final_error)
+        if(final_error > 0.1):
+            self.bakcpropagation(targets)
+            
+    def bakcpropagation(self,targets):
+        #ORDEM DA CAMADA DE SAIDA PARA CAMADA DE ENTRADA
+        self.layers.reverse()
+        
+        self.layers[0].update_output_weights(targets)
+        #print("<",self.layers[0].name)
+        for x in range(1,len(self.layers)): 
+            #print(self.layers[x].name)
+            self.layers[x].update_hidden_weights(self.layers[x-1])
+            
+        #RETIRNA A ORDEM NORMAL
+        #print(">")
+        self.layers.reverse()
+        
+        #SUBSTITUI OS PESOS ANTERIORES PELOS PESOS ATUALIZADOS            
+        [layer.update_weights() for layer in self.layers]
+        
 if __name__ == "__main__":
        
     topology = [4,3,4,2]
     
+    inputs = [[1, 0.05, 0.1, 0.2]]
+    targets = [[0.01, 0.99]]    
+    
     net = Network(topology)
-    net.execute()
+    net.execute(inputs[0],targets[0])
     
     '''
     
--- Iris_Problem/teste.py
+++ Iris_Problem/teste.py
@@ -7,6 +7,6 @@ Created on Tue Oct 18 04:39:23 2016
 
 import math
 
-m = np.array([[1,2,3,4],[5,6,7,8]])
 
-print(m[:,2])
+for x in range(1,5):
+    print(x)
